ff117304f7e8c806b4484bd8b54406e926c34c7a
Andy Harris <andyisrav@gmail.com>
2014-01-07 21:31:36 +0000

    Added Problem 2

---
+++ Problem2/src/Fibonacci.java
@@ -0,0 +1,21 @@
+
+public class Fibonacci {
+
+	
+	static int addEvenNumbers(){
+		int first = 1;
+		int second = 2;
+		int answer;
+		int evenSum = 2;
+		for (answer = first + second; answer <= 4000000; answer = first + second){
+			first = second;
+			second = answer;
+			if (answer%2 == 0){ evenSum += answer; System.out.print(answer + ", ");}
+		}
+		return evenSum;
+	}
+	
+	public static void main (String[] args){
+		System.out.print(addEvenNumbers());
+	}
+}
