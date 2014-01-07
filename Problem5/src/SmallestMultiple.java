2a392d9926196015d4177ddca6ce832f1b08f4ed
ravanova <andyisrav@gmail.com>
2014-01-07 23:04:55 +0000

    Added Problem 5

---
+++ Problem5/src/SmallestMultiple.java
@@ -0,0 +1,33 @@
+
+public class SmallestMultiple {
+
+	public static int findSmallest(){
+		
+		for (int i = 20; true; i+=20){
+			if(i%19 == 0){
+				if(i%18 == 0){
+					if(i%17 == 0){
+						if(i%16 == 0){
+							if(i%15 == 0){
+								if(i%14 == 0){
+									if(i%13 == 0){
+										if(i%12 == 0){
+											if(i%11 == 0){
+												return i;
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		System.out.println(findSmallest());
+	}
+}
