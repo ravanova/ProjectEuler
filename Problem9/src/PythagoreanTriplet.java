258fedc7e7d7137ac68b21c31fc3210105dd5df7
ravanova <andyisrav@gmail.com>
2014-01-08 10:06:26 +0000

    Comments added for clarity

--- Problem9/src/PythagoreanTriplet.java
+++ Problem9/src/PythagoreanTriplet.java
@@ -1,14 +1,14 @@
-
 public class PythagoreanTriplet {
 
 	public static long calculate(){
+		//There exists exactly one Pythagorean triplet for which a + b + c = 1000
+		//This method finds the product abc
 		
 		long aAndB = 0;
 		long cSquared = 0;
 		for(int a = 0; a < 1000; a++){
 			for(int b = a+1; b < 1000; b++){
 				for(int c = b+1; c < 1000; c++){
-					
 					aAndB = (a*a) + (b*b);
 					cSquared = (c*c);
 					if (aAndB == cSquared){
@@ -16,10 +16,8 @@ public class PythagoreanTriplet {
 							return (a*b*c);
 						}
 					}
-					
 				}
 			}
-			
 		}
 		
 		return 0;
