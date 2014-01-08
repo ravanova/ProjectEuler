b755acea649f5717faf20662e57a9299435b6b39
ravanova <andyisrav@gmail.com>
2014-01-08 10:15:23 +0000

    Comments added for clarity

--- Problem10/src/AddingPrimes.java
+++ Problem10/src/AddingPrimes.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 
 
 public class AddingPrimes {
-
+		//The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+		//Find the sum of all the primes below two million.
+		
 	static ArrayList<Integer> divideBy =  new ArrayList<Integer>();
 	static double currentPrime = 1;
 	public  AddingPrimes(){
@@ -14,9 +16,10 @@ public class AddingPrimes {
 		divideBy.add(9);
 	}
 	
-	public static double generatePrime(){
+	public static double generatePrime(){ 
+			//Generates Prime numbers
 		for (int i = (int) (currentPrime+2); i < 2000000; i++){
-			//System.out.println(i);
+			
 			double nonPrime = 0;
 			for (int j = 0; j < divideBy.size(); j++){
 				if (i % divideBy.get(j) == 0) {nonPrime = 1; break;}
@@ -24,11 +27,12 @@ public class AddingPrimes {
 			
 			if (nonPrime == 0){ //Prime number found!
 				divideBy.add(i);
+				//The prime number is added to divideBy in order to ensure that 
+				//multiples of it are not declared as prime in furture iterations
 				currentPrime = i;
 				return i;
 			}
 		}
-		
 		return 0;
 	}
 	
@@ -46,6 +50,5 @@ public class AddingPrimes {
 			//System.out.println(current);
 		}
 		System.out.println(sumOfPrimes+17);
-		
 	}
 }
