ccb20d301093d80d41d0a7d0ed1add4b7918dcdf
ravanova <andyisrav@gmail.com>
2014-01-08 10:02:50 +0000

    Added Problem 9

---
+++ Problem9/src/PythagoreanTriplet.java
@@ -0,0 +1,31 @@
+
+public class PythagoreanTriplet {
+
+	public static long calculate(){
+		
+		long aAndB = 0;
+		long cSquared = 0;
+		for(int a = 0; a < 1000; a++){
+			for(int b = a+1; b < 1000; b++){
+				for(int c = b+1; c < 1000; c++){
+					
+					aAndB = (a*a) + (b*b);
+					cSquared = (c*c);
+					if (aAndB == cSquared){
+						if ((a+b+c) == 1000){
+							return (a*b*c);
+						}
+					}
+					
+				}
+			}
+			
+		}
+		
+		return 0;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(calculate());
+	}
+}
